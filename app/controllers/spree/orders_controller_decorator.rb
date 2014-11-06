09b68a6e911d84f09bd7ff8a3ea9409aaf61e975
Gennady Novoselov <gn@isfb.ru>
2014-11-06 10:34:59 +0300

    BROWNGOODS-220 - Обновление spree до 2.3, рельс до 4.1

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,7 +1,10 @@
 Spree::OrdersController.class_eval do
+
   # Shows the current incomplete order from the session
-  def edit
-    @order = current_order(true)
+  def edit_with_comments
+    edit_without_comments
     @order.comments.build if @order.comments.empty? # At the moment the first user is set to be the creator
   end
+  alias_method_chain :edit, :comments
+
 end
